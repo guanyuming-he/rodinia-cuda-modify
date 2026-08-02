16a0477f0bad356452c1098baea0fdb063bff931
Guanyuming He <guanyuminghe@proton.me>
2026-08-02 22:57:12 +0800

    fix kmeans_clustering for newer compilers.

--- cuda/kmeans/kmeans_clustering.c
+++ cuda/kmeans/kmeans_clustering.c
@@ -71,6 +71,17 @@
 
 #define RANDOM_MAX 2147483647
 
+int	// delta -- had problems when return value was of float type
+kmeansCuda(float  **feature,				/* in: [npoints][nfeatures] */
+           int      nfeatures,				/* number of attributes for each point */
+           int      npoints,				/* number of data points */
+           int      nclusters,				/* number of clusters */
+           int     *membership,				/* which cluster the point belongs to */
+		   float  **clusters,				/* coordinates of cluster centers */
+		   int     *new_centers_len,		/* number of elements in each cluster */
+           float  **new_centers				/* sum of elements in each cluster */
+		   );
+
 extern double wtime(void);
 
 /*----< kmeans_clustering() >---------------------------------------------*/
